ed5e75531097f84c9f40b5b853d758cf5c7f042f
AlbaAstrum <161703485+AlbaAstrum@users.noreply.github.com>
2024-08-29 10:16:27 +0700

    Add files via upload

---
+++ b9n3/src/main/java/com/mycompany/b9n3/B9n3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.b9n3;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class B9n3 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        if (i < 6); {
+        System.out.println(i);
+        }
+        System.out.println("Hasilnya Ialah " + i);
+    }
+}
